7f4c6c07e9ab98ea4f2181cea8f6b875a56639bf
Egor Leonov <leonovegor94@outlook.com>
2023-06-27 13:11:11 +0300

    добавил html файл для представления чат-сервера добавил функционал в work_place - функции и эмпоинты и json в котором будем хранить историю чата

--- work_place.py
+++ work_place.py
@@ -1,25 +1,64 @@
+from flask import Flask, render_template, request  # импортируем Flask
 from datetime import datetime
+import json
 
-all_messages = [] # Здесь будем хранить сообщения
+app = Flask(__name__)  # Создаем экземпляр приложения
+
+
+# json.dump("что", "во что") - загрузка в файл
+# json.load("файл") - чтение файла
+def load_messages():  # для чтения файла с сообщениями
+    with open("s_m.json", "r") as file:
+        data = json.load(file)
+    return data["messages"]
+
+
+all_messages = load_messages()  # Здесь будем хранить сообщения
+
 
 def add_message(author, text):  # обьявляем функцию,
-                                # в которой параметризуем сообщение
+    #                             в которой параметризуем сообщение
     message = {
         "author": author,
         "text": text,
         "time": datetime.now().strftime("%H:%M:%S")
     }
     all_messages.append(message)  # добавляем в список сообщение
+    save_message()
 
-add_message("Сергей", "Привет народ")
-add_message("Max", "Как дела?")
-add_message(text="какие цветочки!", author="Кирилл")
 
-def print_message(msg):  # форматирование сообщения
-    print(f"[{msg['author']}]: {msg['text']} {msg['time']}")
+def save_message():  # обновляем словарь и записываем его в файл
+    all_messages_data = {
+        "messages": all_messages
+    }
+    with open("s_m.json", "w") as file:
+        json.dump(all_messages_data, file)
 
-def print_all_messages():  # вывод отформатированных сообщений
-    for messege in all_messages:
-        print_message(messege)
 
-print_all_messages()
+@app.route("/")  # эмпоинт начальной страницы
+def main_page():
+    return "Hello Egor"
+
+
+@app.route("/chat")
+def chat_page():
+    return render_template("form.html")
+
+
+@app.route("/get_messages")
+def get_messages():
+    print("Выводим все сообщения")
+    return {"messages": all_messages}
+
+
+@app.route("/send_message")
+def send_message():
+    name = request.args.get("name")  # здесь храним данные полученые из query параметров
+    text = request.args.get("text")  # тех, что вводим на страничке (как input)
+    print(f"Пользователь '{name}' пишет '{text}'")
+    add_message(name, text)
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # настраиваем парам. запуска приложения
